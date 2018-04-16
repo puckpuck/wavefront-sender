adac4fae1bae59e5de9a209de469ac94708cfca3
Pierre Tessier <puckpuck.com@gmail.com>
2018-04-16 16:26:06 -0400

    initial commit

---
+++ src/main/java/com/wavefront/labs/sender/MetricPoint.java
@@ -0,0 +1,98 @@
+package com.wavefront.labs.sender;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MetricPoint {
+
+	private String metric;
+	private double value;
+	private long timestamp;
+	private String source;
+	private Map<String, String> tags;
+
+	public MetricPoint() {}
+
+	public MetricPoint(String metric, double value, long timestamp, String source) {
+		this.metric = metric;
+		this.value = value;
+		this.timestamp = timestamp;
+		this.source = source;
+	}
+
+	public MetricPoint(String metric, double value, long timestamp, String source, Map<String, String> tags) {
+		this(metric, value, timestamp, source);
+		this.tags = tags;
+	}
+
+	public String toString() {
+		StringBuilder metricLine = new StringBuilder();
+		metricLine.append(metric);
+		metricLine.append(" ");
+		metricLine.append(value);
+		metricLine.append(" ");
+		metricLine.append(timestamp);
+		metricLine.append(" source=\"");
+		metricLine.append(source);
+		metricLine.append("\"");
+		if (tags != null) {
+			for (Map.Entry<String, String> tag : tags.entrySet()) {
+				metricLine.append(" \"");
+				metricLine.append(tag.getKey());
+				metricLine.append("\"=\"");
+				metricLine.append(tag.getValue());
+				metricLine.append("\"");
+			}
+		}
+
+		return metricLine.toString();
+	}
+
+	public String getMetric() {
+		return metric;
+	}
+
+	public void setMetric(String metric) {
+		this.metric = metric;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public Map<String, String> getTags() {
+		return tags;
+	}
+
+	public void setTags(Map<String, String> tags) {
+		this.tags = tags;
+	}
+
+	public void addTag(String name, String value) {
+		if (tags == null) {
+			tags = new HashMap();
+		}
+
+		tags.put(name, value);
+	}
+}
---
+++ src/main/java/com/wavefront/labs/sender/Wavefront.java
@@ -0,0 +1,137 @@
+package com.wavefront.labs.sender;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.util.Date;
+import java.util.Map;
+import java.util.stream.Stream;
+
+//HACK: This class is not supported in a production environment
+public class Wavefront implements AutoCloseable {
+
+	private static final String DEFAULT_HOST = "localhost";
+	private static final Integer DEFAULT_PORT = 2878;
+
+	private OutputStreamWriter wfWriter;
+
+	private long metricsSent = 0;
+	private long delayEach = 0;
+	private long delayTime = 0;
+
+	public Wavefront() throws IOException {
+		this(DEFAULT_HOST, DEFAULT_PORT);
+	}
+
+	public Wavefront(File file) throws FileNotFoundException {
+		this(file, false);
+	}
+
+	public Wavefront(File file, boolean append) throws FileNotFoundException {
+		wfWriter = new OutputStreamWriter(new FileOutputStream(file, append));
+	}
+
+	public Wavefront(OutputStream outputStream) {
+		wfWriter = new OutputStreamWriter(outputStream);
+	}
+
+	public Wavefront(String host, Integer port) throws IOException {
+		Socket socket = new Socket(host, port);
+		wfWriter = new OutputStreamWriter(socket.getOutputStream(), "ASCII");
+	}
+
+	public void setDelay(long delayEach, long delayTime) {
+		this.delayEach = delayEach;
+		this.delayTime = delayTime;
+	}
+
+	public void sendLine(String metricLine) throws RuntimeException {
+		if (!metricLine.endsWith("\n")) {
+			metricLine += "\n";
+		}
+
+		try {
+			wfWriter.write(metricLine);
+			wfWriter.flush();
+
+			metricsSent++;
+			if (delayEach > 0 && metricsSent % delayEach == 0) {
+				try {
+					System.out.println("Wavefront: " + metricsSent + " metrics sent, delaying: " + delayTime + "ms");
+					Thread.sleep(delayTime);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void sendMetric(com.wavefront.labs.sender.MetricPoint metricPoint) {
+		send(metricPoint.getMetric(), metricPoint.getValue(), metricPoint.getSource(), metricPoint.getTimestamp(), metricPoint.getTags());
+	}
+
+	public void send(String metricName, double value) {
+		send(metricName, value, null);
+	}
+
+	public void send(String metricName, double value, String source) {
+		send(metricName, value, source, new Date().getTime());
+	}
+
+	public void send(String metricName, double value, String source, long timestamp) {
+		send(metricName, value, source, timestamp, "");
+	}
+
+	public void send(String metricName, double value, String source, long timestamp, Map<String, String> tags) {
+
+		StringBuilder sbTags = new StringBuilder();
+		if (tags != null) {
+			for (Map.Entry<String, String> tag : tags.entrySet()) {
+				sbTags.append(" " + tag.getKey() + "=\"" + tag.getValue() + "\"");
+			}
+		}
+
+		send(metricName, value, source, timestamp, sbTags.toString());
+	}
+
+	public void send(String metricName, double value, String source, long timestamp, String tags) {
+		if (source == null || source.equals("")) {
+			try {
+				source = InetAddress.getLocalHost().getHostName();
+			} catch (UnknownHostException e) {
+				source = "localhost";
+			}
+		}
+
+		if (timestamp == 0) {
+			timestamp = new Date().getTime();
+		}
+
+		String metricLine = metricName + " " + value + " " + timestamp + " source=\"" + source + "\" " + tags + "\n";
+		sendLine(metricLine);
+	}
+
+	public void sendFile(String filename) {
+		sendFile(new File(filename));
+	}
+
+	public void sendFile(File file) {
+		try (Stream<String> stream = Files.lines(file.toPath())) {
+			stream.forEach(this::sendLine);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			wfWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
